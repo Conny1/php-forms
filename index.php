7d4f5414be56026f55cde62217e97140780dca62
Conny1 <77896950+Conny1@users.noreply.github.com>
2022-07-17 15:32:23 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,48 @@
 <?php 
+$name = $email = $phoneNumber = '';
 
 $errors = ['name'=>'', 'phoneNumber'=>'', 'email'=>''];
-
+// Featching the values
 if(isset($_POST['submit'])){
+	// name validation
 	if (empty($_POST['name'])) {
 		// code...
 		$errors['name'] = "Name is emply";
+	} else {
+		// code...
+		$name = $_POST['name'];
+		// echo $name ;
 	}
+
+	// email validation
+
 	if (empty($_POST['email'])) {
 		// code...
 		$errors['email'] = "Email is emply";
+	}else {
+		// code...
+		$email = $_POST['email'];
+		// echo $email;
 	}
+
+	// phone number validaation
 	if (empty($_POST['phoneNumber'])) {
 		// code...
 		$errors['phoneNumber'] = "Phone Number is emply";
+
+	}else {
+		// code...
+		$phoneNumber = $_POST['phoneNumber'];
+		// echo $phoneNumber;
 	}
+}
+// cross check for errors
+if(array_filter($errors)){
+	// echo "We still have massive errors";
+}else{
+	// echo 'No errors were found';
+	$email = $phoneNumber =$name ='';
+
 }
 
 
@@ -30,20 +58,20 @@ if(isset($_POST['submit'])){
 
 <form action="index.php" method="POST">
 	<div class="forms"><label for="name" >Input both of your names below</label>
-	<input type="text" name="name" id="name" placeholder="Names" >	
-	<div class="errors"><?php echo $errors['name']; ?></div>
+	<input type="text" name="name" id="name" placeholder="Names" value="<?php echo $name; ?>" >	
+	<div class="errors"><?php echo htmlspecialchars($errors['name']); ?></div>
 	</div>
 
 	<div class="forms">
 		<label for="email" >Email</label>
-		<input type="email" name="email" id="email" placeholder="Enter Email">
-		<div class="errors"><?php echo $errors['email']; ?></div>
+		<input type="email" name="email" id="email" placeholder="Enter Email" value="<?php echo $email; ?>" >
+		<div class="errors"><?php echo htmlspecialchars($errors['email']);  ?></div>
 	</div>
 
 	<div class="forms">
 		<label for="phoneNumber">Phone Number</label>
-		<input type="text" name="phoneNumber" placeholder="phoneNumber" id="phoneNumber">
-		<div class="errors"><?php echo $errors['phoneNumber']; ?></div>
+		<input type="text" name="phoneNumber" placeholder="phoneNumber" id="phoneNumber" value="<?php echo $phoneNumber; ?>">
+		<div class="errors"><?php echo htmlspecialchars($errors['phoneNumber']); ?></div>
 	</div>
 
 	<input type="submit" name="submit" id="submitbtn">
